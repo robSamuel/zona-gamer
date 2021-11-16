c185a4331e689cfba341400f29983c831ade932e
Daniel Jimenez <djimenezsoza18@gmail.com>
2021-11-15 19:40:01 -0600

    arreglo clase conexion

--- ZonaGamer/CapaDatos/Conexion.cs
+++ ZonaGamer/CapaDatos/Conexion.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace CapaDatos
 {
     class Conexion
     {
-        public static string Cn = Properties.Settings.Default.cn;
+        try
+            {
+                connect = new SqlConnection("Server=DESKTOP-4CTFC2J\\SQLSERVER2017;Database=Clientes;UID=" + user + ";PWD=" + pass);
+                connect.Open();
+            }
+            catch (Exception)
+            {
+              
+                
+            }
     }
 }
