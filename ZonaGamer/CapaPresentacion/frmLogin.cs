ca3cb32648fa84b1d102a7ad85b9a8b976834ea3
robSamuel <robert_samuel97@hotmail.com>
2021-12-12 17:27:11 -0600

    Eliminar funcionalidad del progressbar al iniciar sesion

--- ZonaGamer/CapaPresentacion/frmLogin.cs
+++ ZonaGamer/CapaPresentacion/frmLogin.cs
@@ -45,14 +45,6 @@ namespace CapaPresentacion
         private void BtnIngresar_Click(object sender, EventArgs e)
         {
 
-            //Conexion cn = new Conexion();
- 
-                for (int i=0; i<=100; i++)
-            {
-                this.progressBar1.Value = i;
-                Thread.Sleep(100);
-            }
-
             if (intentos != 2)
             {
 
